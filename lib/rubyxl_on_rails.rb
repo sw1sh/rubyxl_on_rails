9e52fdf30d400305962ce439fc801e8a52fd6d2b
Nikolay Murzin <murzin.nikolay@gmail.com>
2015-02-25 13:10:40 +0300

    Main module

--- lib/rubyxl_on_rails.rb
+++ lib/rubyxl_on_rails.rb
@@ -1,5 +1,28 @@
-require "rubyxl_on_rails/version"
+module RubyXLonRails
 
-module RubyxlOnRails
-  # Your code goes here...
+  def self.render_xlsx_string(spreadsheet)
+<<RENDER
+    #{spreadsheet}
+    blob = StringIO.new
+    FileUtils.copy_stream workbook.stream, blob
+    blob.string
+RENDER
+  end
+
+end
+
+# Setups the template handling
+require "action_view/template"
+ActionView::Template.register_template_handler :rxlsx, lambda { |template|
+  RubyXLonRails.render_xlsx_string(template.source)
+}
+
+# Adds support for `format.xlsx`
+require "action_controller"
+require 'rubyXL'
+Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
+
+ActionController::Renderers.add :xlsx do |filename, options|
+  puts options.inspect
+  send_data(render_to_string(options), :filename => "#{filename}.xlsx", :type => "application/xlsx", :disposition => "attachment")
 end
